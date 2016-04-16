cc6e5d8d91d9b892d12edc7a178a3dd4b3d00d62
Kiesum <kieran_summerfeldt@hotmail.com>
2016-04-16 01:07:31 +0000

    added validation part c

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ActiveRecord::Base
 
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :author, presence: true, length: { maximum: 20 }
+  validates :content, presence: true, length: { maximum: 500 }
+
 end
