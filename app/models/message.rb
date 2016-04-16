33c821a37d95f535f92ad402bf0dcc8ab0f5e526
Kiesum <kieran_summerfeldt@hotmail.com>
2016-04-16 02:27:39 +0000

    enhancement_1 done

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,5 @@
 class Message < ActiveRecord::Base
 
-  validates :title, presence: true, length: { maximum: 20 }
   validates :author, presence: true, length: { maximum: 20 }
   validates :content, presence: true, length: { maximum: 500 }
 
